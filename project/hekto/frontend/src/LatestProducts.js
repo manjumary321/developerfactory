683104e7258f0729b2b4b6984eb189eab2895cf8
manjumary321 <106730320+manjumary321@users.noreply.github.com>
2022-09-12 18:05:38 +0530

    leater

--- project/hekto/frontend/src/LatestProducts.js
+++ project/hekto/frontend/src/LatestProducts.js
@@ -1,50 +1,143 @@
 import "./style.css";
 import image1166 from './images/image1166.png'
-function LatestProducts(){
-    return(
+import image15 from './images/image15.png'
+import image1168 from './images/image1168.png'
+import image23 from './images/image23.png'
+import image32 from './images/image32.png'
+import image3 from './images/image3.png'
+import Group27 from './images/Group27.png'
+import Groupcart from './images/Groupcart.png';
+import Vector from './images/Vector.png'
+import uil_search_plus from './images/uil_search_plus.png'
+function LatestProducts() {
+    return (
         <div className="LatestProducts_outer">
             <div className="LatestProducts_header">
                 <label>Leatest Products</label>
             </div>
             <div className="LatestProducts_secondmenu">
-                <div className="LatestProducts_innersecondmenu">
-                    <label className="LatestProducts_Arrival">New Arrival</label>
-                    <label className="LatestProducts_Seller">Best Seller</label>
-                    <label className="LatestProducts_Featured">Featured</label>
-                    <label className="LatestProducts_Offer">Special Offer</label>
-                </div>
+
+                <label className="LatestProducts_secondmenu_col">New Arrival</label>
+                <label className="LatestProducts_secondmenu_col">Best Seller</label>
+                <label className="LatestProducts_secondmenu_col">Featured</label>
+                <label className="LatestProducts_secondmenu_col">Special Offer</label>
+
             </div>
             <div className="LatestProducts_outerimage">
                 <div className="LatestProducts_inneroutimage1">
-                    <div className="LatestProducts_inninnerimage11">
-                        <div className="LatestProducts_image1166" >
-                            <img src={image1166}></img>
-                        </div>  
-                        <div className="LatestProducts_bottomimage1166">
-                            <div className="LatestProducts_botlftimage1166">
+                    <div className="LatestProducts_inninnerimage">
+                        <div className="LatestProducts_inninnerimage_row">
+                            <div className="LatestProducts_imagecol1">
+                                <div className="LatestProducts_imagecol1row1">
+                                    <img src={Group27}></img>
+
+                                </div>
+                                <div className="LatestProducts_imagecol1row2">
+                                    <div className="LatestProducts_imageicons1"><img src={Groupcart}></img></div>
+                                    <div className="LatestProducts_imageicons2"><img src={Vector}></img></div>
+                                    <div className="LatestProducts_imageicons2"><img src={uil_search_plus}></img></div>
+                                </div>
+                            </div>
+                            <div className="LatestProducts_imagecol2" >
+                                <img src={image1166}></img>
+                            </div>
+                        </div>
+                        <div className="LatestProducts_bottomimagecol">
+                            <div className="LatestProducts_botlftimagecol">
                                 <label>Comfort Handy Craft</label>
                             </div>
-                            <div className="LatestProducts_botrgtimage1166">
+                            <div className="LatestProducts_botrgtimagecol">
                                 <label>$42.00</label>
-                                <label>$65.00</label>
+                                <span>$65.00</span>
                             </div>
-                        </div>  
+                        </div>
                     </div>
-                    <div className="LatestProducts_inninnerimage12">
+                    <div className="LatestProducts_inninnerimage">
+                        <div className="LatestProducts_inninnerimage_row">
+                            <div className="LatestProducts_imagecol2" >
+                                <img src={image15}></img>
+                            </div>
+                        </div>
+                        <div className="LatestProducts_bottomimagecol">
+                            <div className="LatestProducts_botlftimagecol">
+                                <label>Comfort Handy Craft</label>
+                            </div>
+                            <div className="LatestProducts_botrgtimagecol">
+                                <label>$42.00</label>
+                                <span>$65.00</span>
+                            </div>
+                        </div>
 
                     </div>
-                    <div className="LatestProducts_inninnerimage13">
+                    <div className="LatestProducts_inninnerimage">
+                        <div className="LatestProducts_inninnerimage_row">
+                            <div className="LatestProducts_imagecol2" >
+                                <img src={image1168}></img>
+                            </div>
+                        </div>
+                        <div className="LatestProducts_bottomimagecol">
+                            <div className="LatestProducts_botlftimagecol">
+                                <label>Comfort Handy Craft</label>
+                            </div>
+                            <div className="LatestProducts_botrgtimagecol">
+                                <label>$42.00</label>
+                                <span>$65.00</span>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
                 <div className="LatestProducts_inneroutimage2">
-                    <div className="LatestProducts_inninnerimage21">
+                    <div className="LatestProducts_inninnerimage">
+                    <div className="LatestProducts_inninnerimage_row">  
+                        <div className="LatestProducts_imagecol" >
+                            <img src={image23}></img>
+                        </div>
+                      </div>  
+                        <div className="LatestProducts_bottomimagecol">
+                            <div className="LatestProducts_botlftimagecol">
+                                <label>Comfort Handy Craft</label>
+                            </div>
+                            <div className="LatestProducts_botrgtimagecol">
+                                <label>$42.00</label>
+                                <span>$65.00</span>
+                            </div>
+                        </div>
+
 
                     </div>
-                    <div className="LatestProducts_inninnerimage22">
+                    <div className="LatestProducts_inninnerimage">
+                    <div className="LatestProducts_inninnerimage_row">
+                        <div className="LatestProducts_imagecol2" >
+                            <img src={image32}></img>
+                        </div>
+                        </div>
+                        <div className="LatestProducts_bottomimagecol">
+                            <div className="LatestProducts_botlftimagecol">
+                                <label>Comfort Handy Craft</label>
+                            </div>
+                            <div className="LatestProducts_botrgtimagecol">
+                                <label>$42.00</label>
+                                <span>$65.00</span>
+                            </div>
+                        </div>
 
                     </div>
-                    <div className="LatestProducts_inninnerimage23">
+                    <div className="LatestProducts_inninnerimage">
+                    <div className="LatestProducts_inninnerimage_row">
+                        <div className="LatestProducts_imagecol2" >
+                            <img src={image3}></img>
+                        </div>
+                        </div>
+                        <div className="LatestProducts_bottomimagecol">
+                            <div className="LatestProducts_botlftimagecol">
+                                <label>Comfort Handy Craft</label>
+                            </div>
+                            <div className="LatestProducts_botrgtimagecol">
+                                <label>$42.00</label>
+                                <span>$65.00</span>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
