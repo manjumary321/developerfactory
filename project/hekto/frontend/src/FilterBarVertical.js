2ed321f8e375b2b4e48b727b1a4f4325d1195ccb
manjumary321 <106730320+manjumary321@users.noreply.github.com>
2022-09-14 22:01:34 +0530

    FilterBarVertical

--- project/hekto/frontend/src/FilterBarVertical.js
+++ project/hekto/frontend/src/FilterBarVertical.js
@@ -1,38 +1,40 @@
 import "./FilterBarVertical.css";
 import FilterBarVertical_search from './Images/FilterBarVertical_search.png';
+import LatestProducts_icons_star1 from './Images/LatestProducts_icons_star1.png';
+import LatestProducts_icons_star2 from './Images/LatestProducts_icons_star2.png'
 
 function FilterBarVertical() {
     return (
         <>
             <div className="FilterBarVertical_outer">
                 {/* ---------Product Brand--------- */}
-                <div className=".FilterBarVertical_ProdBrand">
+                <div className="FilterBarVertical_ProdBrand">
                     <div className="FilterBarVertical_header">
                         <label>Product Brand</label>
                     </div>
-                    <div className="FilterBarVertical_prodcheck">
-                        <div className="FilterBarVertical_prodcheckrow">
+                    <div className="FilterBarVertical_ProdBrand_check">
+                        <div className="FilterBarVertical_ProdBrand_check_row">
                             <input type="checkbox"></input>
                             <label> Coaster Furniture</label>
                         </div>
-                        <div className="FilterBarVertical_prodcheckrow">
+                        <div className="FilterBarVertical_ProdBrand_check_row">
                             <input type="checkbox"></input>
                             <label> Fusion Dot High Fashion</label>
                         </div>
-                        <div className="FilterBarVertical_prodcheckrow">
+                        <div className="FilterBarVertical_ProdBrand_check_row">
                             <input type="checkbox"></input>
                             <label> Unique Furnitture Restor</label>
                         </div>
-                        <div className="FilterBarVertical_prodcheckrow">
+                        <div className="FilterBarVertical_ProdBrand_check_row">
 
                             <input type="checkbox"></input>
                             <label> Dream Furnitture Flipping</label>
                         </div>
-                        <div className="FilterBarVertical_prodcheckrow">
+                        <div className="FilterBarVertical_ProdBrand_check_row">
                             <input type="checkbox"></input>
                             <label> Young Repurposed</label>
                         </div>
-                        <div className="FilterBarVertical_prodcheckrow">
+                        <div className="FilterBarVertical_ProdBrand_check_row">
                             <input type="checkbox"></input>
                             <label> Green DIY furniture</label>
                         </div>
@@ -43,16 +45,16 @@ function FilterBarVertical() {
                     <div className="FilterBarVertical_header">
                         <label>Discount Offer</label>
                     </div>
-                    <div className="FilterBarVertical_DisOffercheck">
-                        <div className="FilterBarVertical_DisOffercheckrow">
+                    <div className="FilterBarVertical_DisOffer_check">
+                        <div className="FilterBarVertical_DisOffer_check_row">
                             <input type="checkbox"></input>
                             <label>20% Cashback</label>
                         </div>
-                        <div className="FilterBarVertical_DisOffercheckrow">
+                        <div className="FilterBarVertical_DisOffer_check_row">
                             <input type="checkbox"></input>
                             <label> 5% Cashback Offer</label>
                         </div>
-                        <div className="FilterBarVertical_DisOffercheckrow">
+                        <div className="FilterBarVertical_DisOffer_check_row">
                             <input type="checkbox"></input>
                             <label> 25% Discount Offer</label>
                         </div>
@@ -63,12 +65,103 @@ function FilterBarVertical() {
                     <div className="FilterBarVertical_header">
                         <label>Rating Item</label>
                     </div>
+                    <div className="FilterBarVertical_RatItem_check">
+                        <div className="FilterBarVertical_RatItem_check_row">
+                            <input type="checkbox"></input>
+                            <div className="FilterBarVertical_RatItem_img_row">
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star2}></img>
+                                <label>(2341)</label>
+                            </div>
+                        </div>
+                        <div className="FilterBarVertical_RatItem_check_row">
+                            <input type="checkbox"></input>
+                            <div className="FilterBarVertical_RatItem_img_row">
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star2}></img>
+                                <img src={LatestProducts_icons_star2}></img>
+                                <label>(1726)</label>
+                            </div>
+                        </div>
+                        <div className="FilterBarVertical_RatItem_check_row">
+                            <input type="checkbox"></input>
+                            <div className="FilterBarVertical_RatItem_img_row">
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star2}></img>
+                                <img src={LatestProducts_icons_star2}></img>
+                                <img src={LatestProducts_icons_star2}></img>
+                                <label>(258)</label>
+                            </div>
+                        </div>
+                        <div className="FilterBarVertical_RatItem_check_row">
+                            <input type="checkbox"></input>
+                            <div className="FilterBarVertical_RatItem_img_row">
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star1}></img>
+                                <img src={LatestProducts_icons_star2}></img>
+                                <img src={LatestProducts_icons_star2}></img>
+                                <img src={LatestProducts_icons_star2}></img>
+                                <label>(25)</label>
+                            </div>
+                        </div>
+                        
+                    </div>
+
                 </div>
                 {/* ---------Categories--------- */}
                 <div className="FilterBarVertical_Categories">
                     <div className="FilterBarVertical_header">
                         <label>Categories</label>
                     </div>
+                    <div className="FilterBarVertical_Cat_check">
+                        <ul>
+                            <li>
+                                <input type="checkbox"></input>
+                                <label> Prestashop</label>
+                            </li>
+                            <li>
+                                <input type="checkbox"></input>
+                                <label> Magento</label>
+                            </li>
+                            <li>
+                                <input type="checkbox"></input>
+                                <label> Bigcommerce</label>
+                            </li>
+                            <li>
+                                <input type="checkbox"></input>
+                                <label> osCommerce</label>
+                            </li>
+                            <li>
+                                <input type="checkbox"></input>
+                                <label> 3dcart</label>
+                            </li>
+                            <li>
+                                <input type="checkbox"></input>
+                                <label> Bags</label>
+                                
+                            </li>
+                                <ul>
+                                    <li>
+                                    <label>Accessories</label>  
+                                    </li>
+                                    <li>
+                                    <label> Bags</label>  
+                                    </li>
+                                </ul>
+                            <li>
+                                <input type="checkbox"></input>
+                                <label> Watches</label>
+                            </li>
+                        </ul>
+
+                    </div>
+
 
                 </div>
                 {/* ---------Price Filter--------- */}
@@ -76,25 +169,25 @@ function FilterBarVertical() {
                     <div className="FilterBarVertical_header">
                         <label>Price Filter</label>
                     </div>
-                    <div className="FilterBarVertical_PriFiltercheck">
-                        <div className="FilterBarVertical_PriFiltercheckrow">
+                    <div className="FilterBarVertical_PriFilt_check">
+                        <div className="FilterBarVertical_PriFilt_check_row">
                             <input type="checkbox" />
                             <span className="geekmark"></span>
                             <label>$0.00 - $150.00</label>
                         </div>
-                        <div className="FilterBarVertical_PriFiltercheckrow">
+                        <div className="FilterBarVertical_PriFilt_check_row">
                             <input type="checkbox" />
                             <label>$150.00 - $350.00</label>
                         </div>
-                        <div className="FilterBarVertical_PriFiltercheckrow">
+                        <div className="FilterBarVertical_PriFilt_check_row">
                             <input type="checkbox" />
                             <label> $150.00 - $350.00</label>
                         </div>
-                        <div className="FilterBarVertical_PriFiltercheckrow">
+                        <div className="FilterBarVertical_PriFilt_check_row">
                         <input type="checkbox" />
                             <label>$450.00 +</label>
                         </div>
-                        <div className="FilterBarVertical_PriFiltersearch">
+                        <div className="FilterBarVertical_PriFilt_search">
                             <input type="name"></input>
                             <img src={FilterBarVertical_search}></img>
 
@@ -106,32 +199,33 @@ function FilterBarVertical() {
                     <div className="FilterBarVertical_header">
                         <label>Filter By Color</label>
                     </div>
-                    <div className="FilterBarVertical_color">
-                        <div className="FilterBarVertical_colorrow">
-                            <div className="FilterBarVertical_colorrow_col1">
+                    <div className="FilterBarVertical_FilByColor_color">
+                        <div className="FilterBarVertical_FilByColor_color_row">
+                            <div className="FilterBarVertical_FilByColor_color_row_col1">
                                 <label>Blue</label>
                             </div>
-                            <div className="FilterBarVertical_colorrow_col2">
+                            <div className="FilterBarVertical_FilByColor_color_row_col2">
                                 <label>Orange</label>
                             </div>
-                            <div className="FilterBarVertical_colorrow_col3">
+                            <div className="FilterBarVertical_FilByColor_color_row_col3">
                                 <label>Brown</label>
                             </div>
                         </div>
-                        <div className="FilterBarVertical_colorrow">
-                            <div className="FilterBarVertical_colorrow_col4">
+                        <div className="FilterBarVertical_FilByColor_color_row">
+                            <div className="FilterBarVertical_FilByColor_color_row_col4">
                                 <label>Green</label>
                             </div>
-                            <div className="FilterBarVertical_colorrow_col5">
+                            <div className="FilterBarVertical_FilByColor_color_row_col5">
                                 <label>Purple</label>
                             </div>
-                            <div className="FilterBarVertical_colorrow_col6">
+                            <div className="FilterBarVertical_FilByColor_color_row_col6">
                                 <label>Sky</label>
                             </div>
 
                         </div>
                     </div>
                 </div>
+                {/* ------------ */}
             </div>
 
         </>
